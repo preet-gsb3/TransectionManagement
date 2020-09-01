605513f14f762f37e0e22f86d6b23052803f3352
Gurpreet Singh <preet.gsb3@gmail.com>
2020-09-01 17:36:50 +0530

    Initial Commit

---
+++ GurpreetTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GurpreetTest
+//
+//  Created by Gurpreet Singh on 01/09/20.
+//  Copyright © 2020 Gurpreet Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
